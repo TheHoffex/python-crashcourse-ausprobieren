5a00e7c52e1443a7bc1abab9762eed01f11b1143
Felix Hoffmann <Felix.Hoffmann.10@gmx.de>
2024-04-20 00:03:51 +0200

    weitergearbeitet

--- game.py
+++ game.py
@@ -14,13 +14,38 @@ class Slither:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    
-
+            self._check_events()
+            self.wurm.update()                 
             pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.wurm.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.wurm.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.wurm.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.wurm.moving_down = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.wurm.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.wurm.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.wurm.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.wurm.moving_down = False
 
 if __name__ == '__main__':
     slither = Slither()
--- wurm.py
+++ wurm.py
@@ -7,8 +7,15 @@ class Wurm:
         self.screen_rect = game.screen.get_rect()
         self.size = 10
         self.speed = 10
-        player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
-        pygame.draw.circle(self.screen, "red", player_pos, self.size)
+        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
+        pygame.draw.circle(self.screen, "red", self.player_pos, self.size)
+
+        self.moving_up = False
+        self.moving_down = False
+        self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        
+        if self.moving_right:
+            self.player_pos.x += self.speed
+        pygame.draw.circle(self.screen, "red", self.player_pos, self.size)
